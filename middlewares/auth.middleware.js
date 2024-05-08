8f71b21d5c07315577933888748595488a4c62fd
Cuong0685123 <eticon0685@gmail.com>
2024-05-08 20:49:23 +0700

    authMiddleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,28 +1,32 @@
-import {NoTokenProvidedError, TokenExpiredError} from "../error/user.error.js";
+import {
+  NoTokenProvidedError,
+  TokenExpiredError,
+} from "../error/user.error.js";
 import jwt from "jsonwebtoken";
 
 const authMiddleware = (req, res, next) => {
-    if(!req.headers.authorization) {
-        throw new NoTokenProvidedError('No token provided');
+  try {
+    if (!req.headers.authorization) {
+      throw new NoTokenProvidedError("No token provided");
+    }
+    const authHeader = req.headers.authorization;
+    const token = authHeader.split(" ")[1];
+
+    if (!token) {
+      throw new NoTokenProvidedError("No token provided");
     }
 
-    const [_, token] = req.headers.authorization.split('Bearer ');
+    const claims = jwt.verify(token, process.env.JWT_SECRET);
+    const { sub } = claims;
 
-    if (!token)
-        throw new NoTokenProvidedError('No token provided');
-
-    try {
-        const claims = jwt.verify(token, process.env.SALT);
-        const {sub} = claims;
-        req.userId = sub;
-
-        next();
-    } catch (error) {
-        if(error.name === 'TokenExpiredError') {
-            throw new TokenExpiredError('Token expired')
-        }
-        throw new NoTokenProvidedError('Token invalid');
+    req.userId = sub;
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      throw new TokenExpiredError("Token expired");
     }
-}
+    throw new NoTokenProvidedError("Token invalid");
+  }
+};
 
 export default authMiddleware;
--- server.js
+++ server.js
@@ -6,11 +6,11 @@ import conversationRouter from './router/conversation.router.js';
 import messageRouter from './router/message.router.js';
 import friendRouter from './router/friend.router.js';
 import cors from 'cors'
+import { RouteNotFoundErrorMiddleware, UnhandleErrorMiddleware } from './middlewares/index.js';
 const PORT = process.env.PORT || 3000;
 
 dotenv.config();
 await connectToMongoDB();
-
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -19,6 +19,8 @@ app.use("/api/conversations", conversationRouter),
 app.use("/api/auth", authRouter),
 app.use("/api/friends", friendRouter),
 
+app.use(UnhandleErrorMiddleware);
+app.use(RouteNotFoundErrorMiddleware);
 
 
 
