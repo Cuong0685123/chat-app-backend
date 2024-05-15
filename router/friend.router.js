0f101c91c9754dbb769f193e7c72ad4bc8f20d9a
Cuong0685123 <eticon0685@gmail.com>
2024-05-16 03:00:46 +0700

    friend

--- router/friend.router.js
+++ router/friend.router.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.post('/',authMiddleware, friendController.add);
 router.put('/:receiverId/receiverId',authMiddleware, friendController.accept);
-router.delete('/:friendId', friendController.delete);
+router.delete('/:friendId/friendId', friendController.delete);
 
 
 
